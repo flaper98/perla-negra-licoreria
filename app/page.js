7205370f01cc368ff7a4f7512a6f59bdec349704
FlavioPerez <fperezha@emeal.nttdata.com>
2026-05-31 19:06:12 -0500

    VERSION2

--- app/page.js
+++ app/page.js
@@ -5,14 +5,17 @@ import Products from "@/components/Products";
 import Benefits from "@/components/Benefits";
 import Footer from "@/components/Footer";
 import FloatingWhatsapp from "@/components/FloatingWhatsapp";
-
+import Offers from "@/components/Offers";
+import { promotions } from "@/data/promotions";
+import CategoryPromotions from "@/components/CategoryPromotions";
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <Offers />
+      <CategoryPromotions />
       <Categories />
-      <Products />
       <Benefits />
       <Footer />
       <FloatingWhatsapp />
---
+++ data/promotions.js
@@ -0,0 +1,34 @@
+export const promotions = [
+  {
+    code: "PROMO 01",
+    title: "Promo Whisky Red Label",
+    category: "Whisky",
+    description: "12 unidades por caja",
+    price: "Consultar",
+    image: "/img/promos/promo-whisky-red-label.jpg",
+  },
+  {
+    code: "PROMO 02",
+    title: "Promo Whisky Black Label",
+    category: "Whisky",
+    description: "12 unidades por caja",
+    price: "Consultar",
+    image: "/img/promotion/whisky/jack-daniels-box.png",
+  },
+  {
+    code: "PROMO 03",
+    title: "Promo Ron Flor de Caña",
+    category: "Ron",
+    description: "12 unidades por caja",
+    price: "Consultar",
+    image: "/img/promos/promo-ron-flor-de-cana.jpg",
+  },
+  {
+    code: "PROMO 04",
+    title: "Promo Vodka Smirnoff",
+    category: "Vodka",
+    description: "12 unidades por caja",
+    price: "Consultar",
+    image: "/img/promos/promo-vodka-smirnoff.jpg",
+  },
+];
