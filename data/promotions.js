bd69d3b332bad0a6d0543113eb5fd0819e4c8b92
FlavioPerez <fperezha@emeal.nttdata.com>
2026-06-02 17:57:25 -0500

    promociones

--- data/promotions.js
+++ data/promotions.js
@@ -1,34 +1,110 @@
 export const promotions = [
+
+  //Whisky
   {
-    code: "PROMO 01",
-    title: "Promo Whisky Red Label",
+    name: "Pack Whisky Premium",
     category: "Whisky",
-    description: "12 unidades por caja",
-    price: "Consultar",
-    image: "/img/promos/promo-whisky-red-label.jpg",
+    image: "/img/promotion/whisky/promo_whsky.png",
+    layout: "vertical"
   },
   {
-    code: "PROMO 02",
-    title: "Promo Whisky Black Label",
+    name: "Pack Jack Daniel's",
     category: "Whisky",
-    description: "12 unidades por caja",
-    price: "Consultar",
-    image: "/img/promotion/whisky/jack-daniels-box.png",
+    image: "/img/promotion/whisky/promo_whisky_2.png",
+    layout: "vertical"
   },
   {
-    code: "PROMO 03",
-    title: "Promo Ron Flor de Caña",
+    name: "Pack Johnnie Walker",
+    category: "Whisky",
+    image: "/img/promotion/whisky/promo_whisky_3.png",
+      layout: "vertical"
+  },
+  {
+    name: "Pack Whisky Comercial",
+    category: "Whisky",
+    image: "/img/promotion/whisky/promo_whisky_4.png",
+    layout: "vertical"
+  },
+  {
+    name: "Pack Whisky Inicio",
+    category: "Whisky",
+    image: "/img/promotion/whisky/promo_whisky_5.png",
+    layout: "vertical"
+  },
+  //ron
+  {
+    name: "Pack Ron Distribuidor",
     category: "Ron",
-    description: "12 unidades por caja",
-    price: "Consultar",
-    image: "/img/promos/promo-ron-flor-de-cana.jpg",
+    image: "/img/promotion/ron/promo_ron.png",
+    layout: "vertical"
   },
   {
-    code: "PROMO 04",
-    title: "Promo Vodka Smirnoff",
-    category: "Vodka",
-    description: "12 unidades por caja",
-    price: "Consultar",
-    image: "/img/promos/promo-vodka-smirnoff.jpg",
+    name: "Pack Ron Premium",
+    category: "Ron",
+    image: "/img/promotion/ron/promo_ron_2.png",
+    layout: "vertical"
   },
+  {
+    name: "Pack Ron Mixto",
+    category: "Ron",
+    image: "/img/promotion/ron/promo_ron_3.png",
+    layout: "vertical"
+  },
+  {
+    name: "Pack Ron Flor de Caña",
+    category: "Ron",
+    image: "/img/promotion/ron/promo_ron_4.png",
+    layout: "vertical"
+  },
+  {
+    name: "Pack Ron Economico",
+    category: "Ron",
+    image: "/img/promotion/ron/promo_ron_5.png",
+    layout: "vertical"
+  },
+  //Vino
+  {
+    name: "Pack Eventos Vinos / Pisco",
+    category: "Vino",
+    image: "/img/promotion/vino/promo_vinos.png",
+    layout: "horizontal"
+  },
+  {
+    name: "Pack Completo Vinos / Espumantes / Pisco",  
+    category: "Vino",
+    image: "/img/promotion/vino/promo_vinos_2.png",
+    layout: "horizontal"
+  },
+  //Vodka
+  {
+    name: "Pack Full Vodka / Cremas",
+    category: "Vodka",    
+    image: "/img/promotion/vodka/promo_vodka.png",
+    layout: "horizontal"
+  },
+  {
+    name: "Pack Crema & Vodka",
+    category: "Vodka",
+    image: "/img/promotion/vodka/promo_vodka_2.png",
+    layout: "horizontal"
+  },
+  //Smirnoff
+  {
+    name: "Pack Smirnoff Premium",
+    category: "Smirnoff",
+    image: "/img/promotion/smirnoff/promo_smirnoff.png",
+    layout: "vertical"
+  },
+  {
+    name: "Pack Smirnoff Comercial",
+    category: "Smirnoff",
+    image: "/img/promotion/smirnoff/promo_smirnoff_2.png",
+    layout: "vertical"
+  },
+  { 
+    name: "Pack Smirnoff Inicio",
+    category: "Smirnoff",
+    image: "/img/promotion/smirnoff/promo_smirnoff_3.png",
+    layout: "vertical"
+  }
 ];
